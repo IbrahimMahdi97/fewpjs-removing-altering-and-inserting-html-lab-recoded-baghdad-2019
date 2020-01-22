80d61c3dfc598d373f33a00a62e653e6cd1c75ba
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-22 16:28:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElemnt('h1');
+ newHeader.setAttribute ('id', 'victory');
+inner = variable.innerHTML ("Ibrahim is the champion");
